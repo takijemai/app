4b299b1b1dd2266d59c7208dedd4534a24f3549f
JEMAI TAKIEDDINE <jt800@inlumine.ual.es>
2022-04-27 04:26:12 +0200

    first commit

--- src/app/addmovie/addmovie.page.ts
+++ src/app/addmovie/addmovie.page.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Camera, CameraResultType } from '@capacitor/camera';
 @Component({
   selector: 'app-addmovie',
   templateUrl: './addmovie.page.html',
   styleUrls: ['./addmovie.page.scss'],
 })
 export class AddmoviePage implements OnInit {
-
-  constructor() { }
+photo: SafeResourceUrl;
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+  }
+  async takePicture() {
+  const image = await Camera.getPhoto({
+  quality: 100,
+  allowEditing: false,
+  resultType: CameraResultType.Uri
+  });
+  this.photo = this.sanitizer.bypassSecurityTrustResourceUrl(image &&
+(image.webPath));
   }
 
 }
